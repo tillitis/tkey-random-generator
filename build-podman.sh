70f219278b687188577a82241d8786ad01fb2733
Michael Cardell Widerkrantz <mc@tillitis.se>
2026-03-12 16:52:56 +0100

    build: Harmonize building scripts with tkey-ssh-agent

--- build-podman.sh
+++ build-podman.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+# SPDX-FileCopyrightText: 2023 Tillitis AB <tillitis.se>
+# SPDX-License-Identifier: BSD-2-Clause
+
 set -eu
 
 tkey_libs_version="v0.0.2"
+
+printf "Building tkey-libs with version: %s\n" "$tkey_libs_version"
 
 if [ -d ../tkey-libs ]
 then
@@ -11,6 +16,6 @@ else
     git clone -b "$tkey_libs_version" https://github.com/tillitis/tkey-libs.git ../tkey-libs
 fi
 
-make -j -C ../tkey-libs
+make -j -C ../tkey-libs podman
 
-make -j tkey-random-generator-dev
+make -j podman
